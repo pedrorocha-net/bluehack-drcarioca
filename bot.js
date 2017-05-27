7167fa45824f971702bb8ff135876908bc9cb8d2
Pedro Rocha <pedrogomesrocha@gmail.com>
2017-05-27 13:31:32 -0300

    clean up

--- bot.js
+++ bot.js
@@ -1,45 +1,33 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Facebook bot built with Botkit.
-
-# RUN THE BOT:
-  Follow the instructions here to set up your Facebook app and page:
-    -> https://developers.facebook.com/docs/messenger-platform/implementation
-  Run your bot from the command line:
-    page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node bot.js
-
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
-
 if (!process.env.page_token) {
-    console.log('Error: Specify a Facebook page_token in environment.');
-    process.exit(1);
+  console.log('Error: Specify a Facebook page_token in environment.');
+  process.exit(1);
 }
 
 if (!process.env.verify_token) {
-    console.log('Error: Specify a Facebook verify_token in environment.');
-    process.exit(1);
+  console.log('Error: Specify a Facebook verify_token in environment.');
+  process.exit(1);
 }
 
 var Botkit = require('botkit');
 var debug = require('debug')('botkit:main');
+var os = require('os');
 
-// Create the Botkit controller, which controls all instances of the bot.
+var page_token;
+
+if (os.hostname().indexOf("local") > -1) {
+  page_token = process.env.page_token_dev1;
+} else {
+  page_token = process.env.page_token;
+}
+
 var controller = Botkit.facebookbot({
-    // debug: true,
-    receive_via_postback: true,
-    verify_token: process.env.verify_token,
-    access_token: process.env.page_token_dev1
+  // debug: true,
+  receive_via_postback: true,
+  verify_token: process.env.verify_token,
+  access_token: page_token
 });
 
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
@@ -59,42 +47,6 @@ require(__dirname + '/components/onboarding.js')(controller);
 require(__dirname + '/components/plugin_dashbot.js')(controller);
 
 var normalizedPath = require("path").join(__dirname, "skills");
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
+require("fs").readdirSync(normalizedPath).forEach(function (file) {
   require("./skills/" + file)(controller);
 });
-
-
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (process.env.studio_token) {
-    controller.on('message_received', function(bot, message) {
-        if (message.text) {
-            controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(function(convo) {
-                if (!convo) {
-                    // no trigger was matched
-                    // If you want your bot to respond to every message,
-                    // define a 'fallback' script in Botkit Studio
-                    // and uncomment the line below.
-                    controller.studio.run(bot, 'fallback', message.user, message.channel);
-                } else {
-                    // set variables here that are needed for EVERY script
-                    // use controller.studio.before('script') to set variables specific to a script
-                    convo.setVar('current_time', new Date());
-                }
-            }).catch(function(err) {
-                if (err) {
-                    bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-                    debug('Botkit Studio: ', err);
-                }
-            });
-        }
-    });
-} else {
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
