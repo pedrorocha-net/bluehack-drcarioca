118fcc8ca7685e21f54fda97fbbb0fa876fa4d3c
Pedro Rocha <pedrogomesrocha@gmail.com>
2017-05-27 11:59:07 -0300

    Corrigindo https://github.com/howdyai/botkit/issues/764

--- components/thread_settings.js
+++ components/thread_settings.js
@@ -5,24 +5,27 @@ var debug = require('debug')('botkit:thread_settings');
 module.exports = function(controller) {
 
     debug('Configuring Facebook thread settings...');
-    controller.api.thread_settings.greeting('Hello! I\'m a Botkit bot!');
-    controller.api.thread_settings.get_started('sample_get_started_payload');
-    controller.api.thread_settings.menu([
-        {
-            "type":"postback",
-            "title":"Hello",
-            "payload":"hello"
-        },
-        {
-            "type":"postback",
-            "title":"Help",
-            "payload":"help"
-        },
-        {
-          "type":"web_url",
-          "title":"Botkit Docs",
-          "url":"https://github.com/howdyai/botkit/blob/master/readme-facebook.md"
-        },
-    ]);
+    controller.api.messenger_profile.greeting('Hello! I\'m a Botkit bot!');
+    controller.api.messenger_profile.get_started('sample_get_started_payload');
+    controller.api.messenger_profile.menu([{
+        "locale":"default",
+        "call_to_actions": [
+            {
+                "type": "postback",
+                "title": "Hello",
+                "payload": "hello"
+            },
+            {
+                "type": "postback",
+                "title": "Help",
+                "payload": "help"
+            },
+            {
+                "type": "web_url",
+                "title": "Botkit Docs",
+                "url": "https://github.com/howdyai/botkit/blob/master/readme-facebook.md"
+            }
+        ]
+    }]);
 
 }
